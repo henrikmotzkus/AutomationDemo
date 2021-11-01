17ccb6ebb7954131a5cecab865591f1cdcc63a1d
Henrik Motzkus <hemotzku@microsoft.com>
2021-11-01 15:57:23 +0100

    initial

---
+++ FunctionCode/deploy/run.ps1
@@ -0,0 +1,31 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$name = $Request.Query.Name
+if (-not $name) {
+    $name = $Request.Body.Name
+}
+
+
+
+
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $name `
+    -TemplateFile '.\ResourceGroup\azuredeploy.json' `
+    -TemplateParameterFile '.\ResourceGroup\azuredeploy.parameters.json'
+
+
+
+$body = "Deployment was successfully"
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+})
