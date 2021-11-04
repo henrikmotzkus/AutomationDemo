a6668eafdaa7e9e5be3328eeeaad203d8c8061a5
Henrik Motzkus <hemotzku@microsoft.com>
2021-11-04 12:30:10 +0100

    cse

--- deploy.ps1
+++ deploy.ps1
@@ -1,74 +1,69 @@
 <#
   Mit Azure Verbinden und Subscription setzen
 #>
-
+$location = "westeurope"
+$subscriptionid = "2abc2ec1-2238-430d-bf52-40cb7dc8b652"
 Connect-AzAccount
-Set-AzContext "2abc2ec1-2238-430d-bf52-40cb7dc8b652"
+Set-AzContext $subscriptionid
 
 
-<#  Diese Aktionen nur einmalig ausführen #>
+
 <#
-    KeyVault für das Adminpasswort anlegen
+    Deployment auf Subscription Ebene
 #>
-New-AzResourceGroup -Name "Keyvault" -Location "westeurope"
+$subrgname = "Deplytosubscription"
+New-AzSubscriptionDeployment `
+    -Name $subrgname `
+    -Location $location `
+    -TemplateFile ".\Subscription\azuredeploy.json"
+
+
+<#
+    Deployment auf ResourceGroup Ebene. Deployt eine VM mit Reference auf einen Keyvault.
+    With conditional varaiables
+#>
+New-AzResourceGroup -Name "Keyvault" -Location $location
 New-AzResourceGroupDeployment `
     -ResourceGroupName "Keyvault" `
     -TemplateFile '.\Keyvault\azuredeploy.json' `
     -adUseriD '49c9b64d-6d4c-4da0-a501-a0d701e1fdf4'
 
-
-<#
-    Azure Function anlegen. Die Azure function stellt eine REST API für das Deployment von Templates bereit
-#>
-New-AzResourceGroup -Name "DeployFunction" -Location "westeurope"
+$deployrgname = "DeploytoRG"
 New-AzResourceGroupDeployment `
-    -ResourceGroupName "DeployFunction" `
-    -TemplateFile '.\Function\azuredeploy.json' `
-    -subscriptionId '2abc2ec1-2238-430d-bf52-40cb7dc8b652' `
-    -FunctionName "AzureDeployFunction" `
-    -location 'westeurope' `
-    -serverFarmResourceGroup 'DeployFunction'
-    
-
-<#
-    Deployment auf Subscription Ebene
-#>
-New-AzSubscriptionDeployment `
-    -Name "Deployresourecgroup" `
-    -Location "westeurope" `
-    -TemplateFile ".\Subscription\azuredeploy.json"
-
-
-
-<#
-    Deployment auf ResourceGroup Ebene
-#>
-New-AzResourceGroupDeployment `
-    -ResourceGroupName "TTest2" `
+    -ResourceGroupName $deployrgname `
     -TemplateFile '.\ResourceGroup\azuredeploy.json' `
     -TemplateParameterFile '.\ResourceGroup\azuredeploy.parameters.json'
     
-    
- 
 
 <#
-    Deployment auf ResourceGroup und Subscription Ebene mit einem nested Template.
+    Deployment auf ResourceGroup UND Subscription Ebene mit einem nested Template. Inline
     Nutzung von Keyvault als Passwort Speicher
 #>
 New-AzSubscriptionDeployment `
     -TemplateFile '.\SubscriptionNested\azuredeploy.json' `
     -TemplateParameterFile '.\SubscriptionNested\azuredeploy.parameters.json' `
-    -Location 'westeurope'
-    
-   
+    -Location $location
+      
 
 <#
-    Deployment eines Templates über eine individuelle REST API. Die Url ist in der Function ersichtlich
+    Deployment eines Templates über eine individuelle REST API. Die Url ist in der Function ersichtlich. 
+    Vorher muss erst die Function angelegt werden.
 #>
+$funcrgname = "DeployFunction"
+New-AzResourceGroup -Name $funcrgname -Location $location
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $funcrgname `
+    -TemplateFile '.\Function\azuredeploy.json' `
+    -subscriptionId $subscriptionid `
+    -FunctionName "AzureDeployFunction" `
+    -location $location `
+    -serverFarmResourceGroup $funcrgname
+    
 Invoke-WebRequest -Uri "https://azuredeployfunction.azurewebsites.net/api/deploy?name=TESTUEBERURL"
 
 <#
-    Deployment über eine eigene UI Definition
+    Deployment über eine eigene UI Definition und automatisch Übergabe ans Azure Portal
+    Vorsicht dieses Feature ist nahezu undokumentiert!
 #>
 https://portal.azure.com/#blade/Microsoft_Azure_CreateUIDef/CustomDeploymentBlade/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fhenrikmotzkus%2FAutomationDemo%2Fmain%2FUIDef%2Fazuredeploy.json/uiFormDefinitionUri/https%3A%2F%2Fraw.githubusercontent.com%2Fhenrikmotzkus%2FAutomationDemo%2Fmain%2FUIDef%2FUIDef.json
 PasswordPassword_
@@ -83,20 +78,33 @@ PasswordPassword_
     Managed Application Demo
     Look: https://docs.microsoft.com/en-us/azure/azure-resource-manager/managed-applications/
 #>
-
+# TODO
 
 <#
-    Blueprint Demo
+    Deploy of a blueprint out of a ARM template.
+    Than manual assigment
 #>
 New-AzSubscriptionDeployment `
   -Name demoDeployment `
-  -Location "westeurope" `
+  -Location $location `
   -TemplateFile .\Blueprints\DeployBlueprint\azuredeploy.json `
   -blueprintName "TestBlueprint"
 
 
 <#
-    Custom Script Extension and Desired State Configuration
-    Mit modularisiertem Deployment "Linked Template"
+    Deployment of a VM to a resource group and a Custom Script Extension 
+    With a linked template
 #>
+$rgname = "DeployVMandCSE2"
+$location = "westeurope"
+New-AzResourceGroup -Name $rgname -Location $location
+New-AzResourceGroupDeployment `
+    -ResourceGroupName $rgname `
+    -TemplateUri "https://raw.githubusercontent.com/henrikmotzkus/AutomationDemo/main/CSEandDSC/azuredeploy.json" `
+    -TemplateParameterUri "https://raw.githubusercontent.com/henrikmotzkus/AutomationDemo/main/CSEandDSC/azuredeploy.parameters.json"
 
+
+
+<#
+    Deploy a Template Specs
+#>
