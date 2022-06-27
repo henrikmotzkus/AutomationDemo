e02ffafe693576ab29ec7f68b0a50f79cb78f753
Henrik Motzkus <hemotzku@microsoft.com>
2022-06-27 16:29:44 +0200

    Step 20 Inlien Deployment script

---
+++ 20_PowerShellinARM/create_Container_Instance.ps1
@@ -0,0 +1,17 @@
+$projectName = "pstestmotzkus"
+$location = "westeurope"
+$templateFile = ".\create_Container_Instance.json"
+$resourceGroupName = "PSTest"
+
+New-AzResourceGroup -Location $location -name $resourceGroupName
+New-AzResourceGroupDeployment -resourceGroupName $resourceGroupName -TemplateFile $templatefile -projectName $projectName
+
+
+
+$fileName = ".\azuredeploy.json"
+
+$storageAccountName = "${projectName}store"
+$fileShareName = "${projectName}share"
+
+$context = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName).Context
+Set-AzStorageFileContent -Context $context -ShareName $fileShareName -Source $fileName -Force
